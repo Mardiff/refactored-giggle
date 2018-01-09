b3154277d64d21491e656b89a272548dbac0c7a0
mardiff <mardiff@github.com>
2018-01-09 00:38:32 -0500

    Adding php call

--- Pitch Calling/USF/create_lineup.js
+++ Pitch Calling/USF/create_lineup.js
@@ -3,10 +3,8 @@ $(document).ready(function(){
 	var options = [];
 
 	$("#parse_batter_names").click(function() {
-
-		alert('asdf');
-		// $.get('create_lineup.php', function(data){
-		// 	alert(data);
-		// });
+		$.get('create_lineup.php', function(data){
+			alert(data);
+		});
 	});
 });
