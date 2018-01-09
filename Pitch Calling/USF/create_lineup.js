5926ecd2fc656e00e44b6b44a97acd1f6fc9dd41
mardiff <mardiff@github.com>
2018-01-09 17:32:58 -0500

    Organization

--- Pitch Calling/USF/create_lineup.js
+++ Pitch Calling/USF/create_lineup.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 
-	var options = [];
+	var options = ['USF_Andrew_Perez.html', 'USF_Cam_Crabbe.html', 'USF_Coco_Montes.html', 'USF_Collin_Sullivan.html', 'USF_David_Villar.html', 'USF_Graham_Hoffman.html', 'USF_Kevin_King.html', 'USF_Kyle_Phillips.html', 'USF_Levi_Borders.html', 'USF_Noah_Yager.html', 'USF_Shane_McLanahan.html'];
 
-	$("#parse_batter_names").click(function() {
-		$.get('create_lineup.php', function(data){
-			alert(data);
-		});
+	options.forEach(function(option){
+		$('select').append('<option>'+option+'</option>');
 	});
+
 });
