860325097f7707cc59f7e73378149164ef84d49b
mardiff <mardiff@github.com>
2018-01-09 00:27:35 -0500

    testing

--- Pitch Calling/USF/create_lineup.js
+++ Pitch Calling/USF/create_lineup.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
 	var options = [];
 
 	$("#parse_batter_names").click(function() {
-		$.get('create_lineup.php', function(data){
-			alert(data);
-		});
+
+		alert('asdf');
+		// $.get('create_lineup.php', function(data){
+		// 	alert(data);
+		// });
 	});
 });
